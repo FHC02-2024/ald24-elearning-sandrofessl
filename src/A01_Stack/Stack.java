8c9c8e336ac5423479d751629588fc0c2791c159
Sandro Fessl <52301605@edu.campus02.at>
2024-11-27 20:36:57 +0100

    Fertigstellung Stack Probleme mit Git deswegen alles auf einmal comittet

--- src/A01_Stack/Stack.java
+++ src/A01_Stack/Stack.java
@@ -1,17 +1,24 @@
+//Sandro Fessl
 package A01_Stack;
 
 
 public class Stack<T>
 {
 	 private Node<T> first;
+    private int cnt;
     /**
      * Oberstes Element entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
      * @throws StackEmptyException 
      */
     public T pop() throws StackEmptyException {
+        if (cnt == 0)
+            throw new StackEmptyException();
 
-    	return null;
+        T data = first.getData();
+        first = first.getNext();
+        cnt--;
+        return data;
     }
     
     /**
@@ -19,7 +26,10 @@ public class Stack<T>
      * @param i data
      */
     public void push(T i) {
-
+        Node<T> newNode = new Node<T>(i);
+        newNode.setNext(first);
+        first = newNode;
+        cnt++;
     }
     
     /**
@@ -27,6 +37,6 @@ public class Stack<T>
      * @return
      */
     public int getCount() {
-    	return 0;
+    	return cnt;
     }
 }
